fc038800cf8e69a8af60fb6731078e892477beec
Naveed Ganjani <nganjani@protonmail.com>
2019-08-16 23:33:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -10,8 +10,8 @@ end
 #Advanced
 def square_with_collect(array)
   new_array = array.collect { |element| element ** 2 }
-  puts new_array
+  return new_array
 end
 
 myarr = [ 2, 4, 6 ]
-square_with_collect(myarr)
+puts square_with_collect(myarr)
